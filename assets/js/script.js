9979f465705392d374ec41e3508c954d0643af46
Bear81 <>
2023-11-27 12:06:46 +0000

    Add - function to generate computer choice
    Added Array to store game options. Used Math.random to generate a random index and used that index to store the computer choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,9 @@ let userChoice;
 let compChoice;
 let result;
 
+// Game options
+const gameOptions = ["rock", "paper", "scissors", "lizard", "spock"]
+
 // Remove Console.Log() after testing
 /**
  * Updates variable userChoice when the user selects their move
@@ -12,7 +15,17 @@ let result;
  */
 function getUserChoice(choice){
     userChoice = choice;
+    getComputerChocie();
     console.log(`User chooses: ${userChoice}`);
 }
 
+// Remove Console.Log() after testing
 
+/**
+ * Generates computer chocie randomly
+ */
+function getComputerChocie(){
+    const randomIndex = Math.floor(Math.random() * gameOptions.length);
+    compChoice = gameOptions[randomIndex];
+    console.log(`Computer chooses: ${compChoice}`)
+}
