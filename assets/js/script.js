1ed12bcaa57291d75a77df2840b3e64f14fa0c0e
Bear81 <>
2023-11-27 11:27:48 +0000

    FIX - filename and storage location updated for /script.js

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Starting Variables
+let userScore = "0";
+let compScore = "0";
+let userChoice;
+let compChoice;
+let result;
+
+// Get user choice
+function getUserChoice(choice){
+    userChoice = choice;
+    console.log(`User chooses: ${userChoice}`);
+}
