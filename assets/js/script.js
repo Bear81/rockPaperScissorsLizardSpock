41b74f024aed3588760b3858f58077327035bc6e
Bear81 <>
2023-11-28 11:41:22 +0000

    Added function to append images to results

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,24 +72,43 @@ function gameWinner() {
 }
 
 function updateGameResult() {
+    //Clear game results container
     const gameResultContainer = document.getElementById("game-result");
     gameResultContainer.innerHTML = "";
+    
+    //Add user results div and text
     const userChoiceDiv = document.createElement("div");
     userChoiceDiv.id = "user-choice";
     const userChoiceHeading = document.createElement("h2");
     // ${userChoice.charAt(0).toUpperCase()}${userChoice.slice(1)}` attribution to 
     userChoiceHeading.textContent = `You chose: ${userChoice.charAt(0).toUpperCase()}${userChoice.slice(1)}`;
+    gameResultContainer.appendChild(userChoiceDiv);
     userChoiceDiv.appendChild(userChoiceHeading);
-    gameResultContainer.appendChild(userChoiceDiv)
-    console.log("updateGameResult() was called")
+    
+    // Add User choice image
+    const userChoiceImg = document.createElement("img");
+    userChoiceImg.id = "user-img";
+    userChoiceImg.alt = "Image of users choice";
+    userChoiceImg.src = "assets/images/lizard.webp";
+    userChoiceDiv.appendChild(userChoiceImg);
+    
+
+    //Add comp results div and text
     const compChoiceDiv = document.createElement("div");
     compChoiceDiv.id = "comp-choice";
     const compChoiceHeading = document.createElement("h2");
     // ${userChoice.charAt(0).toUpperCase()}${userChoice.slice(1)}` attribution to 
     compChoiceHeading.textContent = `Computer chose: ${compChoice.charAt(0).toUpperCase()}${compChoice.slice(1)}`;
-    compChoiceDiv.appendChild(compChoiceHeading);
     gameResultContainer.appendChild(compChoiceDiv);
-    console.log("updateGameResult() was called");
+    compChoiceDiv.appendChild(compChoiceHeading);
+
+    // Add comp choice image
+    const compChoiceImg = document.createElement("img");
+    compChoiceImg.id = "comp-img";
+    compChoiceImg.alt = "Image of computers choice";
+    compChoiceImg.src = "";
+    compChoiceDiv.appendChild(compChoiceImg);
+    
 }
 
 // Event listeners
